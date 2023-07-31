ed5561b96fbb26d1d6d098de9e3ee7c8ac274d30
Norze <123885505+N0rz3@users.noreply.github.com>
2023-07-31 13:20:06 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,6 +6,8 @@ echo Installation requirements
 pip install httpx==0.24.1
 pip install argparse==1.4.0
 pip install folium
+pip install scrape-search-engine==0.2.2
+pip install requests==2.31.0
 
 if %errorlevel% equ 0 (
     echo [+] Installation successful
