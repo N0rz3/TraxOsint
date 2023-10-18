68f1f832358e87fb0eb3d23c3ecc6e5b3030467b
Norze <123885505+N0rz3@users.noreply.github.com>
2023-10-18 20:55:14 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -8,6 +8,7 @@ pip install argparse==1.4.0
 pip install folium
 pip install scrape-search-engine==0.2.2
 pip install requests==2.31.0
+pip install bs4
 
 if %errorlevel% equ 0 (
     echo [+] Installation successful
