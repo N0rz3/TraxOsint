5a1be79ab3e7f8812d7928c571f6c69c83be7211
Norze <123885505+N0rz3@users.noreply.github.com>
2025-08-09 22:31:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ def check_version():
     version = sys.version_info
 
     if (version < (3, 10)):
-        print("[-] Eyes only works with Python 3.10+.")
+        print("[-] Traxosint only works with Python 3.10+.")
         exit("[+] Go install the most recent version of python -> https://www.python.org/downloads/")
 
     import lib.cli
+
     asyncio.run(lib.cli.parser())
